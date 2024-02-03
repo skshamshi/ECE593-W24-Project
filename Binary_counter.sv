99442553c140acbb01b77eb4675b55cee21e306d
korlap2 <126551635+korlap2@users.noreply.github.com>
2024-02-02 23:49:24 -0800

    Update Binary_counter.sv

--- Binary_counter.sv
+++ Binary_counter.sv
@@ -4,7 +4,7 @@ parameter DWIDTH = 9;
 input	logic clk, rst_n;
 input	logic enable;
 output	logic [DWIDTH-1:0]count;
-		logic [DWIDTH-1:0]temp_count;
+	logic [DWIDTH-1:0]temp_count;
 
 always_ff @(posedge clk)
 	if(!rst_n)
