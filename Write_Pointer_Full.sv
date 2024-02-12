a63109823d91cfbc09ccab558335113f24f89cc3
skshamshi <117418724+skshamshi@users.noreply.github.com>
2024-02-11 23:10:42 -0800

    Update Write_Pointer_Full.sv

--- Write_Pointer_Full.sv
+++ Write_Pointer_Full.sv
@@ -1,17 +1,15 @@
-module Write_Ptr(wclk, wrst_n, w_enable, rgptr_sync, wptr, full);
+module Write_Ptr #(parameter ADDRWIDTH = 9, parameter DWIDTH = 8) (top_if.Write_Pointer_inf wr_if, 
+					input	logic [ADDRWIDTH-1:0]rgptr_sync,
+					output	logic [ADDRWIDTH-1:0]wptr,
+					output	logic [ADDRWIDTH-1:0]wg_ptr	
+					);
 
-parameter ADDRWIDTH = 9;
-input	logic wclk, wrst_n, w_enable;
-input	logic [ADDRWIDTH-1:0]rgptr_sync;
-output	logic [ADDRWIDTH-1:0]wptr;
-output	logic [ADDRWIDTH-1:0]wg_ptr;
-output	logic full;
-		logic [ADDRWIDTH-1:0]rbptr_sync;
-		logic [ADDRWIDTH-1:0]wb_ptr;
-		
-assign full = {~wb_ptr[ADDRWIDTH-1],wb_ptr[ADDRWIDTH-2:0]} == rbptr_sync;
+logic [ADDRWIDTH-1:0]rbptr_sync;
+logic [ADDRWIDTH-1:0]wb_ptr;
+	
+assign wr_if.full = wr_if.wrst_n ? ({~wb_ptr[ADDRWIDTH-1],wb_ptr[ADDRWIDTH-2:0]} == rbptr_sync) : 0;
 
-Binary_counter BinaryCounter(.count(wb_ptr), .clk(wclk), .rst_n(wrst_n), .enable(w_enable));
+Binary_counter BinaryCounter(.count(wb_ptr), .clk(wr_if.wclk), .rst_n(wr_if.wrst_n), .enable(wr_if.w_enable));
 
 Binary_Gray BtoG(.Binary_in(wb_ptr), .Gray_out(wg_ptr));
 
