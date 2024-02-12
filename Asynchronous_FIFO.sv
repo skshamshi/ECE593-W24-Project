707270a649eded0eac18b1c9867c527d25e5378c
korlap2 <126551635+korlap2@users.noreply.github.com>
2024-02-11 23:11:32 -0800

    Update Asynchronous_FIFO.sv

--- Asynchronous_FIFO.sv
+++ Asynchronous_FIFO.sv
@@ -1,28 +1,19 @@
-module Asynchronous_FIFO(wdata, wclk, w_enable, wrst_n, rclk, r_enable, rrst_n, full, empty, rdata);
+module Asynchronous_FIFO #(parameter ADDRWIDTH = 9, parameter DWIDTH = 8) (top_if fifo_if);
 
-parameter DWIDTH	= 8;
-parameter ADDRWIDTH = 9;
+logic [ADDRWIDTH-1:0]rgptr_sync, wgptr_sync;
+logic [ADDRWIDTH-1:0]wg_ptr, rg_ptr;
+logic [ADDRWIDTH-1:0]wptr, rptr;
 
-input	logic wclk, w_enable;
-input	logic rclk, r_enable;
-input	logic [DWIDTH-1:0]wdata;
-input	logic wrst_n, rrst_n;
-output	logic [DWIDTH-1:0]rdata;
-output	logic full, empty;
-		logic [ADDRWIDTH-1:0]waddr, raddr;
-		logic [ADDRWIDTH-1:0]rgptr_sync, wgptr_sync;
-		logic [ADDRWIDTH-1:0]wg_ptr, rg_ptr;
-		logic [ADDRWIDTH-1:0]wptr, rptr;
-		
 
-Write_Ptr Producer(.wclk(wclk), .wrst_n(wrst_n), .w_enable(w_enable), .rgptr_sync(rgptr_sync), .wg_ptr(wg_ptr), .wptr(wptr), .full(full));
 
-FIFO_Memory Memory(.wdata(wdata), .waddr(wptr), .wclk(wclk), .w_enable(w_enable), .full(full), .rclk(rclk), .raddr(rptr), .r_enable(r_enable), .empty(empty), .rdata(rdata));
+Write_Ptr Producer(fifo_if.Write_Pointer_inf, rgptr_sync, wptr, wg_ptr);
 
-Read_Ptr Consumer(.rclk(rclk), .rrst_n(rrst_n), .r_enable(r_enable), .wgptr_sync(wgptr_sync), .rg_ptr(rg_ptr), .rptr(rptr), .empty(empty));
+FIFO_Memory Memory(fifo_if.FIFO_Memory_inf, wptr, rptr);
 
-Synchronizer_2FF Write_read(.clk(rclk), .rst_n(rrst_n), .d_in(wg_ptr), .d_out(wgptr_sync));
+Read_Ptr Consumer(fifo_if.Read_ptr_inf, wgptr_sync, rptr, rg_ptr);
 
-Synchronizer_2FF Read_Write(.clk(wclk), .rst_n(wrst_n), .d_in(rg_ptr), .d_out(rgptr_sync));
+Synchronizer_2FF Write_read(.clk(fifo_if.rclk), .rst_n(fifo_if.rrst_n), .d_in(wg_ptr), .d_out(wgptr_sync));
+
+Synchronizer_2FF Read_Write(.clk(fifo_if.wclk), .rst_n(fifo_if.wrst_n), .d_in(rg_ptr), .d_out(rgptr_sync));
 
 endmodule
