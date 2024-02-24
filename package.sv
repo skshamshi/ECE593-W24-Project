acfab84acaf60a137bfd472cf8717d8ec57bd3ad
skshamshi <117418724+skshamshi@users.noreply.github.com>
2024-02-23 18:48:33 -0800

    Add files via upload

---
+++ package.sv
@@ -0,0 +1,18 @@
+package fifo_package;
+
+typedef enum {STIMULUS, RESET}Pkt_type;
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "iMonitor.sv"
+`include "oMonitor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+`include "test.sv"
+//`include "top.sv"
+
+
+
+
+
+endpackage
