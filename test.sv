f356d09af41d6ef2ec930efd6e0d13e8ccff1569
skshamshi <117418724+skshamshi@users.noreply.github.com>
2024-02-23 18:45:18 -0800

    Update test.sv

--- test.sv
+++ test.sv
@@ -1,27 +1,23 @@
+import fifo_package::*;
+typedef environment env_t;
 class test;
  bit [4:0] no_of_pkts;
- virtual memory_if.tb     vif;
- virtual memory_if.tb_mon_in vif_mon_in;
- virtual memory_if.tb_mon_out vif_mon_out;
+ virtual top_if vif;
  
- environment env;
+ env_t env;
   
- function new (input virtual memory_if.tb vif_in,
-               input virtual memory_if.tb_mon_in vif_mon_in,
-               input virtual memory_if.tb_mon_out vif_mon_out);
+ function new (input virtual top_if vif_in);
  	this.vif= vif_in;
- 	this.vif_mon_in=vif_mon_in;
- 	this.vif_mon_out=vif_mon_out;
  endfunction
  
  function void build();
- 	env = new(vif,vif_mon_in,vif_mon_out,no_of_pkts);
+ 	env = new(vif,no_of_pkts);
  	env.build();
  endfunction
   
  task run ();
  	$display("[Testcase] run started at time=%0t",$time);
- 	no_of_pkts= 30;
+ 	no_of_pkts = 30;
  	build();
  	env.run();
  	$display("[Testcase] run ended at time=%0t",$time);
